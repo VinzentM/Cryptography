3eb14c75065f3afc42c01cc4defee7dc800bcf4f
VinzentM <vinzentmoesch@hanovernorwichschools.org>
2016-10-11 21:03:15 -0400

    Updated from Brython Server: 10/11/2016 9:03:14 PM

--- cryptography.py
+++ cryptography.py
@@ -12,7 +12,7 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 counter = 0
 lisas = []
-while counter < 84:
+while counter < 85:
     lisas.append(associations[counter])
     counter+=1
 aslis = list(range(0,86))
